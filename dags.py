5e314203ec0b83dc73de49c0e26b8ddb2639b3e8
hurivono <jhyoon9966@gmail.com>
2024-11-20 22:14:30 +0900

    Update dags.py

--- dags.py
+++ dags.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 6, 29),
+    'start_date': datetime(2024, 11, 20),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
